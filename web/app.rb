454a1f8a53f8cd4141cf4882f28caf89ebf777c9
ddddddO <lbfdeatq@gmail.com>
2019-04-30 23:09:18 +0900

    add error handling & view

--- web/app.rb
+++ web/app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/base'
 require 'sinatra/reloader'
 
+
 get '/' do
   'sinatra!!!!!'
 end
@@ -13,4 +14,19 @@ end
 get '/list' do
   @foo = 'get_list'
   erb :'/memo/list'
+end
+
+# client error
+error 400..499 do
+  #status = response.status
+  @e = "クライアントエラー: #{response.status}"
+  @msg = "正しい操作をしてください。"
+  erb :error
+end
+
+# server error
+error 500..599 do
+  @e = "サーバーエラー: #{response.status}"
+  @msg = "管理者に連絡してください。"
+  erb :error
 end
