162ec144b3a01925bb951559a3c7b9be0e927526
ddddddO <lbfdeatq@gmail.com>
2020-11-01 20:18:12 +0900

    Fix マシンタイプのコスト削減

--- deployments/main.tf
+++ deployments/main.tf
@@ -27,11 +27,20 @@ resource "google_sql_database_instance" "instance" {
   database_version = "POSTGRES_11"
   region           = "asia-northeast1"
   settings {
-    #tier = "db-f1-micro"
-    tier = "db-custom-1-4096"
+    tier = "db-f1-micro"
+    #tier = "db-custom-1-4096"
     maintenance_window {
       day  = 1
       hour = 0
+    }
+    ip_configuration {
+      ipv4_enabled = true
+      require_ssl  = false
+
+      authorized_networks {
+        name  = "hugo-generator"
+        value = "35.226.69.24/32"
+      }
     }
   }
 }
