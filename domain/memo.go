a1c799ff7217cd5f0e7c36d147549aeac8a3688a
ddddddO <lbfdeatq@gmail.com>
2021-04-18 11:58:21 +0900

    Modify memo domain

--- domain/memo.go
+++ domain/memo.go
@@ -1,13 +1,20 @@
 package domain
 
+import (
+	"time"
+)
+
 // Memo is ...
 type Memo struct {
-	ID          int    `json:"id"`
-	Subject     string `json:"subject"`
-	Content     string `json:"content"`
-	IsExposed   bool   `json:"is_exposed"`
-	UserID      int    `json:"user_id"`
-	Tags        []Tag  `json:"tags"`
-	NotifiedCnt int    `json:"notified_cnt"`
-	RowVariant  string `json:"_rowVariant"` // for vue
+	ID          int        `json:"id"`
+	Subject     string     `json:"subject"`
+	Content     string     `json:"content"`
+	IsExposed   bool       `json:"is_exposed"`
+	UserID      int        `json:"user_id"`
+	Tags        []Tag      `json:"tags"`
+	NotifiedCnt int        `json:"notified_cnt"`
+	CreatedAt   *time.Time `json:"created_at"`
+	UpdatedAt   *time.Time `json:"updated_at"`
+	ExposedAt   *time.Time `json:"exposed_at"`
+	RowVariant  string     `json:"_rowVariant"` // for vue
 }
